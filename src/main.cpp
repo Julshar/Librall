2b46ac731f78f32040e98ee5975d6dbd48c924d3
Julshar <ju.chmielarski@gmail.com>
2025-06-11 18:43:06 +0200

    Split testing into testing and speed testing

--- src/main.cpp
+++ src/main.cpp
@@ -15,60 +15,62 @@ void sortingAlgsTest()
 {
   Random::seedWithTime();
 
-  std::vector<int> smallArray = VectorUtil::generateRandomVector(10, 0, 100);
-  std::vector<int> largeArray = VectorUtil::generateRandomVector(100000, 0, 100000);
+  std::vector<int> arr = VectorUtil::generateRandomVector(40, 0, 100);
 
-  std::cout << "Small array: ";
-  VectorUtil::printVector(smallArray);
+  std::cout << "Unsorted array: ";
+  VectorUtil::printVector(arr);
 
-  std::vector<int> smallArrayCopy_1 = smallArray; // Copy for sorting
-  std::vector<int> smallArrayCopy_2 = smallArray; // Copy for sorting
-  std::vector<int> smallArrayCopy_3 = smallArray; // Copy for sorting
-  std::vector<int> smallArrayCopy_4 = smallArray; // Copy for sorting
+  std::vector<int> smallArrayCopy_1 = arr; // Copy for sorting
+  std::vector<int> smallArrayCopy_2 = arr; // Copy for sorting
+  std::vector<int> smallArrayCopy_3 = arr; // Copy for sorting
+  std::vector<int> smallArrayCopy_4 = arr; // Copy for sorting
 
   SortAlgs::insertionSort(smallArrayCopy_1);
   std::cout << "TEST: Sorted small array (insertion): ";
   VectorUtil::printVector(smallArrayCopy_1);
 
   SortAlgs::mergeSort(smallArrayCopy_2);
-  std::cout << "TEST: Sorted small array (merge): ";
+  std::cout << "TEST: Sorted small array (merge):     ";
   VectorUtil::printVector(smallArrayCopy_2);
 
   SortAlgs::quickSort(smallArrayCopy_3);
-  std::cout << "TEST: Sorted small array (quick): ";
+  std::cout << "TEST: Sorted small array (quick):     ";
   VectorUtil::printVector(smallArrayCopy_3);
 
   SortAlgs::heapSort(smallArrayCopy_4);
-  std::cout << "TEST: Sorted small array (heap): ";
+  std::cout << "TEST: Sorted small array (heap):      ";
   VectorUtil::printVector(smallArrayCopy_4);
+}
 
-  std::vector<int> largeArrayCopy_1 = largeArray; // Copy for sorting
-  std::vector<int> largeArrayCopy_2 = largeArray; // Copy for sorting
-  std::vector<int> largeArrayCopy_3 = largeArray; // Copy for sorting
-  std::vector<int> largeArrayCopy_4 = largeArray; // Copy for sorting
+void sortingAlgsSpeedTest()
+{
+  Random::seedWithTime();
+
+  std::vector<int> arr = VectorUtil::generateRandomVector(100000, 0, 100000);
+
+  std::vector<int> largeArrayCopy_1 = arr; // Copy for sorting
+  std::vector<int> largeArrayCopy_2 = arr; // Copy for sorting
+  std::vector<int> largeArrayCopy_3 = arr; // Copy for sorting
+  std::vector<int> largeArrayCopy_4 = arr; // Copy for sorting
 
   Timer timer;
 
   SortAlgs::insertionSort(largeArrayCopy_1);
-
   std::cout << "SPEED TEST: Insertion sort:\n";
   timer.printElapsedTimeSeconds();
   timer.reset();
 
   SortAlgs::mergeSort(largeArrayCopy_2);
-
   std::cout << "SPEED TEST: Merge sort:\n";
   timer.printElapsedTimeSeconds();
   timer.reset();
 
   SortAlgs::quickSort(largeArrayCopy_3);
-
   std::cout << "SPEED TEST: Quick sort:\n";
   timer.printElapsedTimeSeconds();
   timer.reset();
 
   SortAlgs::heapSort(largeArrayCopy_4);
-
   std::cout << "SPEED TEST: Heap sort:\n";
   timer.printElapsedTimeSeconds();
 }
@@ -81,11 +83,18 @@ int main(int argc, char *argv[])
 
   auto* vbox = new QVBoxLayout(&window);
 
-  auto* btnQuickSort = new QPushButton("Run sorting algorithms test", &window);
-  vbox->addWidget(btnQuickSort);
-  QObject::connect(btnQuickSort, &QPushButton::clicked, []()
+  auto* btnSortTest = new QPushButton("Sorting algorithms test", &window);
+  vbox->addWidget(btnSortTest);
+  QObject::connect(btnSortTest, &QPushButton::clicked, []()
   {
     sortingAlgsTest();
+  });
+
+  auto* btnSortSpeedTest = new QPushButton("Sorting algorithms speed test", &window);
+  vbox->addWidget(btnSortSpeedTest);
+  QObject::connect(btnSortSpeedTest, &QPushButton::clicked, []()
+  {
+    sortingAlgsSpeedTest();
   });
 
   window.setLayout(vbox);
