a5bbdf96a61ea8895ce36d7ed3a12be7e22cf54a
Julshar <ju.chmielarski@gmail.com>
2025-06-14 13:51:57 +0200

    Thread count is now implementation dependent

--- src/util/ParallelSortAlgs.cpp
+++ src/util/ParallelSortAlgs.cpp
@@ -1,5 +1,12 @@
 #include "ParallelSortAlgs.h"
 
+#include <cmath> // for std::log2
+#include <iostream> // std::cout
+
+#undef DEBUG
+
+int ParallelSortAlgs::maxDepth = 0; // Initialize maxDepth
+
 void ParallelSortAlgs::merge(std::vector<int>& arr, int left, int mid, int right)
 {
   int n1 = mid - left + 1, n2 = right - mid;
@@ -20,7 +27,7 @@ void ParallelSortAlgs::mergeSortPrv(std::vector<int>& arr, int left, int right, 
   {
     int mid = left + (right - left) / 2;
 
-    if (depth < MAX_DEPTH)
+    if (depth < maxDepth)
     {
       std::thread t1(&ParallelSortAlgs::mergeSortPrv, std::ref(arr), left, mid, depth + 1);
       std::thread t2(&ParallelSortAlgs::mergeSortPrv, std::ref(arr), mid + 1, right, depth + 1);
@@ -39,5 +46,10 @@ void ParallelSortAlgs::mergeSortPrv(std::vector<int>& arr, int left, int right, 
 
 void ParallelSortAlgs::mergeSort(std::vector<int>& arr)
 {
+  ParallelSortAlgs::maxDepth = std::thread::hardware_concurrency() ? std::log2(std::thread::hardware_concurrency()) : 2;
+  #ifdef DEBUG
+  std::cout << "ParallelSortAlgs::mergeSort: Using max depth: " << ParallelSortAlgs::maxDepth << "\n";
+  std::cout << "ParallelSortAlgs::mergeSort: std::thread::hardware_concurrency(): " << std::thread::hardware_concurrency() << "\n";
+  #endif
   mergeSortPrv(arr, 0, arr.size() - 1);
 }
--- src/util/ParallelSortAlgs.h
+++ src/util/ParallelSortAlgs.h
@@ -14,5 +14,6 @@ private:
   static void mergeSortPrv(std::vector<int>& arr, int left, int right, int depth = 0);
   static void merge(std::vector<int>& arr, int left, int mid, int right);
 
-  static const int MAX_DEPTH = 3;
+  static const int MAX_DEPTH = 3; // Not used currently. Const is preffered for clarity, but this value can differ based on implementation
+  static int maxDepth;
 };
