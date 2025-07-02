caa4fa1930ba56a89be4d181a0035678f4e94d40
Julshar <ju.chmielarski@gmail.com>
2025-07-02 15:23:11 +0200

    Tooltips are now adjusted for dark mode

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,17 @@
 int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
+
+  app.setStyleSheet(R"(
+    QToolTip {
+      color: white;
+      background-color: #333;
+      border: 1px solid white;
+      padding: 2px;
+      border-radius: 3px;
+    }
+  )");
+  
   MainWindow window;
   window.show();
   return app.exec();
